5bcb5cd4203b8b59c5a5899651eed36d78e4b3ab
mdshafii786 <mdshafiii959@gmail.com>
2025-09-18 12:27:59 +0530

    Update UsersControllers.cs

--- UsersControllers.cs
+++ UsersControllers.cs
@@ -1,1 +1,29 @@
 
+using MyWebAPI.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace MyWebAPI.Controllers
+{
+    public class UsersController : ApiController
+    {
+        // Temporary in-memory storage
+        private static List<User> users = new List<User>();
+
+        // GET api/users
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            return users;
+        }
+
+        // POST api/users
+        [HttpPost]
+        public IHttpActionResult AddUser(User user)
+        {
+            if (user == null) return BadRequest("Invalid user data");
+            users.Add(user);
+            return Ok(user);
+        }
+    }
+}
